25c0dd83bbc4aad2fe1d5b938a6fbeb3dfe8dfe5
Vostanis <kimonvostanis@gmail.com>
2024-01-13 20:36:56 +0000

    so close..

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # THIS BUILD IS UNSAFE
 
-get_vec() is a useful fn that allows for async get requests of urls in a Vec<&str> form, e.g.
+get_vec() allows for async get requests of urls in a Vec<&str> form, e.g.
 
     let urls = vec![
         "www.endpoint_1.xml",
         "www.endpoint_2.json"
     ];
     get_vec(urls, "./src", 2).await;
+    
+AIM FOR A STRUCT REMAKE WITH A MACRO:
+
+    let runner = Runner::new();
+
+    #[header("User-Agent", "example@email_domain.com")]
+    runner.get_vec(urls, ...).await;
+
 
 This would retrieve both files in the urls vec, and download them to the "src/" directory, using 2 async threads.
 
--- blacksmith_macros/src/lib.rs
+++ blacksmith_macros/src/lib.rs
@@ -2,20 +2,41 @@
 #[allow(dead_code)]
 #[allow(unused_imports)]
 
-// use std::collections::HashSet as Set;
-use proc_macro::{TokenStream, 
-    // TokenTree, Punct, Literal
-};
-use quote::quote;
+use proc_macro::TokenStream;
+// use quote::quote;
 use syn::{
     parse_macro_input, 
-    Token, 
-    // ExprMethodCall, 
+    Token,
     Lit,
-    // Meta,
     parse::{ParseStream, Parse, Result},
     punctuated::Punctuated,
 };
+
+/// Example use:
+///
+///     #[header($NAME, $VALUE)]
+///     $IDENT.get_vec($URLS, $SAVE_PATH, $THREADS).await;
+///
+#[proc_macro_attribute]
+pub fn header(attr: TokenStream, item: TokenStream) -> TokenStream {
+
+    // 1. ensure 2 strings as args
+    let args = Args::return_vec_of_2(parse_macro_input!(attr as Args));
+    let name = &args[0];
+    let val = &args[1];
+    println!("{:?}, {:?}", name, val);
+
+    // 2. find identity of method call
+    let item_clone = item.clone();
+    let method = parse_macro_input!(item_clone as syn::Expr);
+
+    println!("{method:#?}");
+
+    // 3. insert header to HeaderMap, call the fn, then remove the header
+    // let clone = item.clone();
+    // let parsed = parse_macro_input!(clone as ExprMethodCall);
+    item
+}
 
 // parsing macro inputs
 #[derive(Clone, Debug)]
@@ -47,62 +68,3 @@ impl Args {
 }
 
 
-
-#[proc_macro_attribute]
-pub fn header2(attr: TokenStream, item: TokenStream) -> TokenStream {
-
-    let args = Args::return_vec_of_2(parse_macro_input!(attr as Args));
-    let name = &args[0];
-    let val = &args[1];
-    println!("{:?}, {:?}", name, val);
-    
-    // let clone = item.clone();
-    // let parsed = parse_macro_input!(clone as ExprMethodCall);
-    item
-}
-
-
-
-
-
-
-
-
-
-    // let header: (String, String) = { 
-    //     let mut attrs_iter = attr.into_iter();
-    //     let name = expect_literal(&mut attrs_iter).to_string();
-    //     let _ = expect_punct(&mut attrs_iter, ',');
-    //     let value = expect_literal(&mut attrs_iter).to_string();
-    //     (name, value)
-    // };
-
-// parsing attributes 1 by 1 as iterator (see "parse arguments at top")
-// fn expect_literal(lex: &mut impl Iterator<Item=TokenTree>) -> Literal {
-//     match lex.next() {
-//         Some(TokenTree::Literal(literal)) => literal,
-//         Some(_token) => panic!("expected Literal token but got something else"),
-//         None => panic!("expected literal but got nothing"),
-//     }
-// }
-//
-// fn expect_punct(lex: &mut impl Iterator<Item=TokenTree>, ch: char) -> Punct {
-//     match lex.next() {
-//         Some(TokenTree::Punct(punct)) => if punct.as_char() == ch {
-//             punct
-//         } else {
-//             panic!("Expected punctuation {expected}, but got {actual}",
-//                    expected = ch, actual = punct.as_char())
-//         },
-//         Some(_token) => panic!("Expected punct got something else"),
-//         None => panic!("Expected punct but got nothing")
-//     }
-// }
-
-// fn expect_ident(lex: &mut impl Iterator<Item=TokenTree>) -> Ident {
-//     match lex.next() {
-//         Some(TokenTree::Ident(ident)) => ident,
-//         Some(_) => panic!("Expected ident but got something else"),
-//         None => panic!("Expected ident but got nothing")
-//     }
-// }
